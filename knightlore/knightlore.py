1b12f688f11ab37b90f856ca8d210909a6703e05
mikerr <mike@redrobe.net>
2025-01-17 17:03:43 +0000

    hours of daylight and night , moon = wolf

--- knightlore/knightlore.py
+++ knightlore/knightlore.py
@@ -2,18 +2,9 @@
 
 import pygame
 import time,random
-
 import RPi.GPIO as GPIO         
 
-buttons = {
-           "A":5,
-           "B":6,
-           "UP":17,
-           "DOWN":22,
-           "LEFT":27,
-           "RIGHT":23,
-           "CENTER":4
-}
+buttons = { "A":5, "B":6, "UP":17, "DOWN":22, "LEFT":27, "RIGHT":23, "CENTER":4 }
 WIDTH = HEIGHT = 240
 # zx spectrum colors
 BLACK = (0,0,0)
@@ -30,18 +21,19 @@ class spriteobj:
     color = WHITE
     flip = 0
     x = y = 0
-    jumping = 0
     height = 0
+    jumping = 0
     xdir = ydir = 0.5
+    facing  = 0
     time = grabbed = 0
     img = [0,0]
     w = h = 32
 
 def iso2screen (x,y):
-     # convert to isometric 
-     isox = 100 + int ( 11 * (x - y))
-     isoy = 20 + int ( 6 * (x + y ))
-     return (isox,isoy)
+    # convert to isometric 
+    isox = 100 + int ( 11 * (x - y))
+    isoy = 20 + int ( 6 * (x + y ))
+    return (isox,isoy)
 
 def getsprite(spritesheet,x,y,w,h):
     sprite = pygame.Surface([w,h])
@@ -49,13 +41,13 @@ def getsprite(spritesheet,x,y,w,h):
     return sprite
 
 def blitsprite (spritesheet,sprite,mirror = 0):
-     r,g,b = sprite.color
+    r,g,b = sprite.color
 
-     isox, isoy = iso2screen(sprite.x,sprite.y)
-     isoy -= sprite.height
-     image = getsprite(spritesheet,sprite.img[0],sprite.img[1],sprite.w,sprite.h)
-     image.fill((r,g,b,255), special_flags=pygame.BLEND_RGBA_MIN)
-     screen.blit(pygame.transform.flip(image,mirror,False),(isox,isoy),(0,0,sprite.w,sprite.h))
+    isox, isoy = iso2screen(sprite.x,sprite.y)
+    isoy -= sprite.height
+    image = getsprite(spritesheet,sprite.img[0],sprite.img[1],sprite.w,sprite.h)
+    image.fill((r,g,b,255), special_flags=pygame.BLEND_RGBA_MIN)
+    screen.blit(pygame.transform.flip(image,mirror,False),(isox,isoy),(0,0,sprite.w,sprite.h))
     
 def pressed(btn) :
     return ( GPIO.input(buttons[btn]) != True) 
@@ -76,64 +68,77 @@ sabreman = spriteobj()
 sabreman.img = [0,32]
 sabreman.w = 23
 
-man = 0
+hours = 0
+moon = 0
 def update () :
-    global firing,wolf,man
-    
-    if (pressed("A") and sabreman.jumping == 0 ):
+    global hours,moon
+    if (pressed("B") and sabreman.jumping == 0 ):
        sabreman.jumping = 30
     
     # change to a wolf
-    #if (wolf) : man += 64
+    wolf = 64
+    if (moon) : man = wolf
+    else : man = 0
     
-    speed = 0.05
+    speed = 0.1
     if (pressed("DOWN")):
-        sabreman.img[1] = man + 32 #facing front
-        sabreman.ydir += speed
+        sabreman.ydir = speed
         sabreman.flip  = 1
+        sabreman.facing = 32
     if (pressed("RIGHT")):
-        sabreman.img[1] = man + 32  # facing front
-        sabreman.xdir += speed
+        sabreman.xdir = speed
         sabreman.flip  = 0
+        sabreman.facing = 32
         
     if (pressed("UP")):
-        sabreman.img[1] = man # facing away
-        sabreman.ydir -= speed
+        sabreman.ydir = -speed
         sabreman.flip  = 1
+        sabreman.facing = 0
     if (pressed("LEFT")):
-        sabreman.img[1] = man # facing away
-        sabreman.xdir -= speed
-  
+        sabreman.xdir = -speed
+        sabreman.flip  = 0
+        sabreman.facing = 0
+    man += sabreman.facing
+ 
     # keep on 8x8 grid
     if (sabreman.x < 0) : sabreman.x = 0 ; sabreman.xdir = 0
     if (sabreman.x > 8) : sabreman.x = 8 ; sabreman.xdir = 0
     if (sabreman.y < 0) : sabreman.y = 0 ; sabreman.ydir = 0
     if (sabreman.y > 8) : sabreman.y = 8 ; sabreman.ydir = 0
 
+    sabreman.img[1] = man
+
     sabreman.x += sabreman.xdir 
     sabreman.y += sabreman.ydir
 
     sabreman.xdir *= 0.7
     sabreman.ydir *= 0.7
-
-    stopped =  speed  / 2 
+    if abs(sabreman.ydir) < speed  / 2 : sabreman.ydir = 0
+    if abs(sabreman.xdir) < speed  / 2 : sabreman.xdir = 0
 
     if (sabreman.jumping > 0) :
         sabreman.jumping -= 1
         if (sabreman.jumping > 15):  sabreman.height = 30 - sabreman.jumping  
         if (sabreman.jumping < 15): sabreman.height = sabreman.jumping
-    else: sabreman.height = 0
-        
-    if (abs(sabreman.xdir) > stopped or abs(sabreman.ydir) > stopped) : ## walking
+    if (abs(sabreman.xdir) > 0 or abs(sabreman.ydir) > 0) : ## walking
         frame = sabreman.img[0]
         if frame < 100 : frame += 24
         else : frame = 0
         sabreman.img[0] = frame
+    hours += 0.01
+    if hours > 12 : 
+        hours = 0
+        moon = 1 - moon
 
 def draw () :
-        screen.blit(backdrop,(0,0))
-        # sabreman
-        blitsprite(spritesheet,sabreman,sabreman.flip)
+    global hours,moon
+    screen.blit(backdrop,(0,0))
+    
+    if (moon) : color = WHITE
+    else : color = YELLOW
+    pygame.draw.circle(screen,color,(180 + int(hours * 2) ,170), 5)
+
+    blitsprite(spritesheet,sabreman,sabreman.flip)
         
 while True:
     update()

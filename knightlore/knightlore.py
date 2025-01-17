ac520443ee10e037785b26b6abcedc3780f26ce9
mikerr <mike@redrobe.net>
2025-01-17 15:09:50 +0000

    convert to pygame

--- knightlore/knightlore.py
+++ knightlore/knightlore.py
@@ -1,85 +1,133 @@
 # knightlore - isometric game
 
+import pygame
 import time,random
-from readbmp import *
+
+import RPi.GPIO as GPIO         
+
+buttons = {
+           "A":5,
+           "B":6,
+           "UP":17,
+           "DOWN":22,
+           "LEFT":27,
+           "RIGHT":23,
+           "CENTER":4
+}
+WIDTH = HEIGHT = 240
+
+BLACK = (0,0,0)
+BLUE = (0,0,255)
+RED = (255,0,0)
+MAGENTA = (255,0,255)
+GREEN = (0,255,0)
+YELLOW = (255,255,0)
+CYAN = (0,255,255)
+WHITE = (255,255,255)
+colors = (BLACK,BLUE,RED,MAGENTA,CYAN,YELLOW,WHITE)
+
 
 class spriteobj:
-    x = y = 20
+    color = WHITE
+    flip = 0
+    x = y = 0
     xdir = ydir = 0.5
     time = grabbed = 0
     img = [0,0]
     w = h = 32
 
+def iso2screen (x,y):
+     # convert to isometric 
+     isox = 100 + int ( 11 * (x - y))
+     isoy = 20 + int ( 6 * (x + y ))
+     return (isox,isoy)
+
+def getsprite(spritesheet,x,y,w,h):
+    sprite = pygame.Surface([w,h])
+    sprite.blit(spritesheet,(0,0),(x,y,w,h))
+    return sprite
+
 def blitsprite (spritesheet,sprite,mirror = 0):
-     offset = 0
-     if sprite.h > 8 : offset = 8  - sprite.h
-     blit(spritesheet,sprite.img[0],sprite.img[1],sprite.w,sprite.h,int(sprite.x),int(sprite.y) + offset,sprite.w,sprite.h,mirror)
+     r,g,b = sprite.color
+
+     isox, isoy = iso2screen(sprite.x,sprite.y)
+     image = getsprite(spritesheet,sprite.img[0],sprite.img[1],sprite.w,sprite.h)
+     image.fill((r,g,b,255), special_flags=pygame.BLEND_RGBA_MIN)
+     screen.blit(pygame.transform.flip(image,mirror,False),(isox,isoy),(0,0,sprite.w,sprite.h))
     
 # initialize
-spritebuffer = readbmp ("sabreman.bmp")
+# setup gpio
+GPIO.setmode(GPIO.BCM)         
+for btn in buttons:
+    GPIO.setup(buttons[btn], GPIO.IN)
+def pressed(btn) :
+    return ( GPIO.input(buttons[btn]) != True) 
 
-screensize = 120
-ground = 110
+screen = pygame.display.set_mode()
+pygame.mouse.set_visible(False) 
+
+spritesheet = pygame.image.load("sabreman.bmp")
+backdrop = pygame.image.load("backdrop.png")
+
 sabreman = spriteobj()
 sabreman.img = [0,32]
 sabreman.w = 23
 
 man = 0
-def update (ticks) :
+def update () :
     global firing,wolf,man
     
-    
-    if (button(A)) : firing  = 1
+    if (pressed("A")) : firing  = 1
     else : firing = 0
     
-    if (button(Y)) : man = 65 - man
+    if (pressed("A")) : man = 65 - man
     
     wolfimg = 64
     
-    if (button(DOWN)):
+    speed = 0.05
+    if (pressed("DOWN")):
         sabreman.img[1] = man + 32 #facing front
-        sabreman.xdir -= 1
-        sabreman.ydir += 1
-    if (button(RIGHT)):
+        sabreman.ydir += speed
+        sabreman.flip  = 1
+    if (pressed("RIGHT")):
         sabreman.img[1] = man + 32  # facing front
-        sabreman.xdir += 1
-        sabreman.ydir += 1
+        sabreman.xdir += speed
+        sabreman.flip  = 0
         
-    if (button(UP)):
+    if (pressed("UP")):
         sabreman.img[1] = man # facing away
-        sabreman.ydir -= 1
-        sabreman.xdir += 1
-    if (button(LEFT)):
+        sabreman.ydir -= speed
+        sabreman.flip  = 1
+    if (pressed("LEFT")):
         sabreman.img[1] = man # facing away
-        sabreman.xdir -= 1
-        sabreman.ydir -= 1
-    
-    sabreman.xdir *= 0.7
-    sabreman.ydir *= 0.7
+        sabreman.xdir -= speed
+  
     # keep on screen / wrap left/right
-    sabreman.y = min(ground,sabreman.y)
-    if (sabreman.x < 0 or sabreman.x > screensize) : sabreman.x = screensize - sabreman.x
-    sabreman.y = max(0,sabreman.y)
-        
+    if (sabreman.x < 0) : sabreman.x = 0 ; sabreman.xdir = 0
+    if (sabreman.x > 8) : sabreman.x = 8 ; sabreman.xdir = 0
+    if (sabreman.y < 0) : sabreman.y = 0 ; sabreman.ydir = 0
+    if (sabreman.y > 8) : sabreman.y = 8 ; sabreman.ydir = 0
+
     sabreman.x += sabreman.xdir 
     sabreman.y += sabreman.ydir
-  
-    if (abs(sabreman.xdir) > 0.5) : ## walking
+
+    sabreman.xdir *= 0.7
+    sabreman.ydir *= 0.7
+
+    stopped =  speed  / 2 
+    if (abs(sabreman.xdir) > stopped or abs(sabreman.ydir) > stopped) : ## walking
         frame = sabreman.img[0]
         if frame < 100 : frame += 24
         else : frame = 0
         sabreman.img[0] = frame
 
-def draw (ticks) :
-        pen(0,0,0)
-        clear()   
-        
+def draw () :
+        screen.blit(backdrop,(0,0))
         # sabreman
-        mirror = 0
-        if (sabreman.xdir < 0 and sabreman.ydir > 0) : mirror = HFLIP
-        if (sabreman.xdir > 0 and sabreman.ydir < 0) : mirror = HFLIP
-        #else : mirror = 0
-        blitsprite(spritebuffer,sabreman,mirror)
+        blitsprite(spritesheet,sabreman,sabreman.flip)
         
-        flip()
-start()
+while True:
+    update()
+    draw()
+    pygame.display.flip()
+    pygame.time.wait(10)
